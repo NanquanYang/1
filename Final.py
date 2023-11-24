d6bb6635a4c57143586934955518e62aa264f94e
杨南泉 <yangnanquan@yangnanquansAir.modem>
2023-11-24 16:03:16 +1100

    Initial commit

---
+++ Final.py
@@ -0,0 +1,22 @@
+# import pandas as pd
+#
+# index = [1999,2000,2001,2002,2003]
+# ticker = ['A','A','A','B','B']
+# ret = [0.1, 0.1, 0.1, 0.4, 0.4]
+#
+# df = pd.DataFrame({'ticker':ticker,'ret':ret},index=index)
+#
+# print(df)
+# print(df.groupby('ticker').last())
+# print(df.iloc[:,0])
+#
+# x=[1,2]
+# y =x
+# y.append(3)
+# print(y)
+
+x = 0
+if t:
+    x = x + 1
+
+print(x)
